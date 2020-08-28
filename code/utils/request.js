55f7bb12ed982f37b8595a6acf786d4a1645677a
古晓辉 <xiaohui@guxiaohuideMacBook-Pro.local>
2020-08-28 11:37:46 +0800

    change origin

--- code/utils/request.js
+++ code/utils/request.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const request = axios.create({
-  baseURL: "https://conduit.productionready.io",
+  // baseURL: "https://conduit.productionready.io",
+  baseURL: "http://realworld.api.fed.lagounews.com",
   headers: {
     Authorization: `Token eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTA5Nzk3LCJ1c2VybmFtZSI6InhpYW9odWkiLCJleHAiOjE2MDI2NDMzMjl9.p99bgvhGi6tWmBbqTF2yKNo3mPjYOfOfqpJLCZbj_mY`,
   },
