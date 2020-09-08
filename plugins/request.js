433543ac9ff757d2f0d8a0df93296b6a985e7f6e
古晓辉 <xiaohui@guxiaohuideMacBook-Pro.local>
2020-09-08 16:33:35 +0800

    change host

--- plugins/request.js
+++ plugins/request.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const request = axios.create({
-  baseURL: "http://realworld.api.fed.lagounews.com",
+  baseURL: "https://conduit.productionready.io",
 });
 
 export default ({ store }) => {
